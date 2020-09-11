f92cca3be2b088ed7ab9203fdb221b0cc920ef40
Derek <derekb0147@gmail.com>
2020-09-11 17:41:23 -0400

    new milestone

--- src/main.rs
+++ src/main.rs
@@ -45,21 +45,31 @@ enum InputMode {
     Description,
 }
 
+enum ActiveWidget {
+    Tab,
+    Input,
+    Lane,
+    Card,
+}
+
 struct App {
     input: String,
     input_mode: InputMode,
+    active_widget: ActiveWidget,
     goals: Vec<Goal>,
     lanes: Vec<Lane>,
     cards: Vec<Card>,
 }
+
 impl Default for App {
     fn default() -> App {
         App {
             input: String::new(),
             input_mode: InputMode::Normal,
-            goals: vec![],
-            lanes: vec![],
-            cards: vec![],
+            active_widget: ActiveWidget::Input,
+            goals: Vec::new(),
+            lanes: Vec::new(),
+            cards: Vec::new(),
         }
     }
 }
@@ -81,15 +91,53 @@ fn main() -> Result<(), io::Error> {
         //let (title, description) = match app.input_mode {}
         //let cards;
 
+        /*
+         * The input box is a 'paragraph' widget
+         * we set the value of the paragraph to the value of some string
+         * this is what makes it look like we're typing
+         * 
+         * We'll need to position the cursor one space after the String
+         *
+         * learn to change between 'widgets'
+         * and handle the same key in a different way depending on
+         * the 'active' widget
+         */
+
+        // Create a small help message
+        // Create an empty string that represents out input
+        
+        // Display the cursor
+        match app.input_mode {
+            // The cursor is hidden by default on the alt screen
+            // so we don't actually need to do anything for the
+            // case of Normal mode
+            InputMode::Normal => {},
+            InputMode::Title |
+            InputMode::Description => {
+                // Show the cursor
+                f.set_cursot()
+            },
+        } 
+
+        // Draw the layout
+        terminal.draw(|f| {
+            // Put everything else in here
+            // to the point where I need the small help menu
+            // the input
+            // the lanes w/ lists in them
+            // and the description box
+            // drawn on the screen.
+        })
+
         // Handle input
-        if let Event::Input(input) = events.next().expect("") {
+        if let Event::Input(input) = events.next().unwrap() {
             match app.input_mode {
                 InputMode::Normal => match input {
                     Key::Char('t') => {
-                        print!("Do stuff with the t key\r\n");
+                        print!("Do stuff with the t key\n\r");
                     },
                     Key::Char('d') => {
-                        println!("Do stuff with the d key");
+                        print!("Do stuff with the d key\n\r");
                     },
                     Key::Char('q') => {
                         break;
