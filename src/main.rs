c8f318f5f48e18a9c9a166eb99af94817a40915f
Derek Barnes <dsbarnes@Dereks-iMac.attlocal.net>
2020-09-11 09:57:43 -0400

    Handling input on a very basic level

--- src/main.rs
+++ src/main.rs
@@ -19,43 +19,94 @@ use tui::{
     Terminal,
 };
 
+struct Lane {
+    name: String,
+}
+
+struct Card {
+    title: String,
+    description: String,
+    priority: u8,
+    time: u8,
+    // due: timestamp,
+    lane: Lane,
+}
+
+struct Goal {
+    title: String,
+    description: String,
+    lanes: Vec<Lane>,
+    cards: Vec<Card>,
+}
+
+enum InputMode {
+    Normal,
+    Title,
+    Description,
+}
+
+struct App {
+    input: String,
+    input_mode: InputMode,
+    goals: Vec<Goal>,
+    lanes: Vec<Lane>,
+    cards: Vec<Card>,
+}
+impl Default for App {
+    fn default() -> App {
+        App {
+            input: String::new(),
+            input_mode: InputMode::Normal,
+            goals: vec![],
+            lanes: vec![],
+            cards: vec![],
+        }
+    }
+}
+
+
 fn main() -> Result<(), io::Error> {
-    /*
-     * Tabs, one for each goal
-     * Input for Card title and Card description
-     * Kanban Sections
-     * Cards
-     *
-     * */
-    struct Lane {
-        name: String,
-    }
+    let app = App::default();
+    let events = Events::new();
 
-    struct Card {
-        title: String,
-        description: String,
-        priority: u8,
-        time: u8,
-        // due: timestamp,
-        lane: Lane,
-    }
-
-    struct Goal {
-        title: String,
-        description: String,
-        lanes: Vec<Lane>,
-        cards: Vec<Card>,
-    }
-
-    enum InputMode {
-        Normal,
-        Title,
-        Description,
-    }
+    // The double stdout is what the actual documentation suggests
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
     loop {
-        println!("In here we handle the things...");
-        break;
+        //let chunks;
+        //let help_message;
+        //let (title, description) = match app.input_mode {}
+        //let cards;
+
+        // Handle input
+        if let Event::Input(input) = events.next().expect("") {
+            match app.input_mode {
+                InputMode::Normal => match input {
+                    Key::Char('t') => {
+                        print!("Do stuff with the t key\r\n");
+                    },
+                    Key::Char('d') => {
+                        println!("Do stuff with the d key");
+                    },
+                    Key::Char('q') => {
+                        break;
+                    },
+                    _ => {},
+                },
+                InputMode::Title => match input {
+                    Key::Char('q') => {
+                        break;
+                    },
+                    _ => {},
+                },
+                InputMode::Description => {
+                    break;
+                },
+            }
+        }
     }
 
     // So rust doesn't complain
